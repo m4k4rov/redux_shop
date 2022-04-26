8b1471a6acd4f51785729d818cfdcfbf15faa932
m4k4rov <makarov.pav.al@gmail.com>
2022-04-26 11:08:38 +0300

    edit header link

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -4,12 +4,21 @@ import './app-header.scss';
 import { Link } from 'react-router-dom';
 
 const AppHeader = ({total}) => {
+
+    const addClass = (e) => {
+        let headerLink = document.querySelectorAll('.header__link');
+        headerLink.forEach(item => {
+            item.classList.remove('active');
+        });
+        e.target.classList.add('active');
+    }
+
     return (
         <header className="header">
-            <Link className="header__link" to='/'>
+            <Link onClick={e => addClass(e)} className="header__link active" to='/'>
                 Menu
             </Link>
-            <Link className="header__link" to='cart'>
+            <Link onClick={e => addClass(e)} className="header__link" to='cart'>
                 <img className="header__cart" src={cartIcon} alt="cart"></img>
                 Total: {total} $
             </Link>
